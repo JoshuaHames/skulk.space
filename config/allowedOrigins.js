d6adfce84c514e7716e58abfbe10fa2614c8412a
Ergo Bytes <joshuachames@gmail.com>
2025-03-05 17:09:05 -0700

    Fixed CORS Options

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'https://www.yoursite.com',
     'http://127.0.0.1:5500',
-    'http://localhost:32001'
+    'http://localhost:32000'
 ];
 
 module.exports = allowedOrigins;
